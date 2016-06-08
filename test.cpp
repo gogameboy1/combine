516a9b3ccf2fea43c7f2cad0ba8e59271fdaec95
Johnny <gogameboy11@gmail.com>
2016-06-08 13:46:50 +0800

    combine c pipe and OpenCV together

--- test.cpp
+++ test.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
 
 using namespace std;
 using namespace cv;
 
 int main() {
+
+    int pipe_fds[2];
+    pid_t pid;
+
+    pipe(pipe_fds);
+
+    char *cmd[] = {"raspivid" , "-t 0 -w 1280 -h 720 -fps 60 -hf -b 2000000 -o -" , 0};
+
+    if((pid = fork()) == 0)
+    {
+      dup2(pipe_fds[1] , 1);
+      close(pipe_fds[0]);
+      execvp(cmd[0] , cmd);
+
+      perror(cmd[0]);
+      exit(0);
+    }else if(pid == -1)
+    {
+      perror("fork");
+      exit(1);
+    }
+  
+    dup2(pipe_fds[0] , 0);
+    close(pipe_fds[1]);
 
     cout <<"enter main"<< endl;
     VideoCapture cap;
